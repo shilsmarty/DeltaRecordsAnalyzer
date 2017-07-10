b2c78c81e077015083db275dbe0e6ec69a053b1b
shilsmarty <shiladitya.srivastava@gmail.com>
2017-07-10 00:02:23 -0700

    Adding Delta Records Analyzer initial checkin.

---
+++ DeltaRecordAnalyzer/DeltaRecordAnalyzer/ItemInventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DeltaRecordAnalyzer
+{
+    public class ItemInventory : IEquatable<ItemInventory>
+    {
+
+        public string ItemCode { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int ItemCost { get; set; }
+
+        public bool Equals(ItemInventory other)
+        {
+            //Check whether the compared object is null.
+            if (Object.ReferenceEquals(other, null)) return false;
+
+            //Check whether the compared object references the same data.
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            //Check whether the properties are equal.
+            return ItemCode.Equals(other.ItemCode) && ItemName.Equals(other.ItemName) && ItemCost.Equals(other.ItemCost);
+        }
+
+        public override int GetHashCode()
+        {
+
+            int hashItemCode = ItemCode.GetHashCode();
+
+            int hashItemName = ItemName?.GetHashCode() ?? 0;
+
+            int hashItemCost = ItemCost.GetHashCode();
+
+            //Calculate the hash code.
+            return (hashItemCode + hashItemName) ^ (hashItemCost);
+        }
+    }
+}
---
+++ DeltaRecordAnalyzer/DeltaRecordAnalyzer/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DeltaRecordAnalyzer
+{
+    /// <summary>
+    /// Delta Records Analyzer using Hashset O(1) performance + object hashing GetHashCode() magic.
+    /// </summary>
+    public class Program
+    {
+        static readonly Random _random = new Random();
+
+        public static void Main(string[] args)
+        {
+            int times = 10000000;
+
+            HashSet<ItemInventory> hashsetA = new HashSet<ItemInventory>();
+            HashSet<ItemInventory> hashsetB;
+            string[] itemNameList =
+            {
+                "Paper", "Bag", "Pen", "Sharpner", "Pencil", "Rubber", "Book", "Uniform",
+                "TiffinBox"
+            };
+
+            Console.WriteLine(" Starting the delta records analyzer test tool. ");
+
+            Console.WriteLine(" Total load of records in this test is : "+ times);
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < times; i++)
+            {
+                hashsetA.Add(new ItemInventory()
+                {
+                    ItemCode = string.Concat(GetLetter(), _random.Next(times)),
+                    ItemName = itemNameList[_random.Next(itemNameList.Length)],
+                    ItemCost = _random.Next(),
+                });
+
+                if (i == 1000)
+                    hashsetA.Add(new ItemInventory()
+                    {
+                        ItemCode = "HashsetA - TestCode",
+                        ItemName = "HashsetA - TestName",
+                        ItemCost = _random.Next(),
+                    });
+            }
+
+            hashsetB =  new HashSet<ItemInventory>(hashsetA);
+
+            var itemInventories = hashsetB.ToList();
+
+            //Updation of an existing element in hashsetB.
+            //------------------------------------------------------
+            var deltaIndex = _random.Next(times);
+            var addingDeltaChanges = new ItemInventory()
+            {
+                ItemCode = "HashSetB - ChangedCode",
+                ItemName = "HashSetB - ChangedName",
+                ItemCost = 50,
+            };
+            itemInventories[deltaIndex] = addingDeltaChanges;
+
+            var addingChanges = new ItemInventory()
+            {
+                ItemCode = "HashsetB - TestCode",
+                ItemName = "HashsetB - TestName",
+                ItemCost = deltaIndex,
+            };
+            itemInventories[1001] = addingChanges;
+
+            //Adding new element in HashSetB
+            //-----------------------------------------------------
+            itemInventories.Add(new ItemInventory()
+            {
+                ItemCode = "HashSetB - NewFirstCode",
+                ItemName = "HashSetB - NewFirstName",
+                ItemCost = 100
+            });
+
+            itemInventories.Add(new ItemInventory()
+            {
+                ItemCode = "HashSetB - NewSecondCode",
+                ItemName = "HashSetB - NewSecondName",
+                ItemCost = 100
+            });
+
+            //Adding new element in HashSetA
+            //-----------------------------------------------------
+            hashsetA.Add(new ItemInventory()
+            {
+                ItemCode = "HashSetA - NewCode",
+                ItemName = "HashSetA - NewName",
+                ItemCost = 100
+            });
+
+            hashsetB = new HashSet<ItemInventory>(itemInventories);
+
+            //Magic is happening here.
+            hashsetA.SymmetricExceptWith(hashsetB);
+
+            timer.Stop();
+
+            Console.WriteLine(" Identified records which are delta in the 2 hashset : " + hashsetA.Count + Environment.NewLine);
+
+            //Display the delta records.
+            Console.WriteLine("Different Records : " + Environment.NewLine);
+
+            foreach (var deltaRow in hashsetA)
+            {
+                Console.WriteLine(deltaRow.ItemCode + " , " + deltaRow.ItemName + " , " + deltaRow.ItemCost + " , " + Environment.NewLine);
+            }
+
+            Console.WriteLine(timer.Elapsed.Seconds  + " total time elapsed in seconds.");
+
+            Console.ReadLine();
+        }
+
+        public static char GetLetter()
+        {
+            // This method returns a random lowercase letter.
+            // ... Between 'a' and 'z' inclusize.
+            int num = _random.Next(0, 26); // Zero to 25
+            return (char) ('A' + num);
+        }
+    }
+}
